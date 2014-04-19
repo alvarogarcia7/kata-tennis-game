aff0badef1e5a9d20b8dcc39937bd2f4e8bd9108
Alvaro Garcia <derecerca@gmail.com>
2014-04-19 15:50:40 +0200

    green

--- src/Tennis.js
+++ src/Tennis.js
@@ -3,23 +3,39 @@ var Tennis = function(){
 	this.SECOND_POINT = 30;
 	this.THIRD_POINT = 40;
 	this.ADVANTAGE = "ADV"; 
+	this.WON = "WON"; 
 	this.scorePlayerOne = 0;
 	this.scorePlayerTwo = 0;
 };
 
 Tennis.prototype.scoreOne = function(){
 	this.scorePlayerOne = this.calculateNewScore(this.scorePlayerOne);
+	this.normalizeScore();
 }
 
 Tennis.prototype.scoreTwo = function(){
 	this.scorePlayerTwo = this.calculateNewScore(this.scorePlayerTwo);
+	this.normalizeScore();
+}
+
+Tennis.prototype.normalizeScore = function(){
+	if(this.ADVANTAGE === this.scorePlayerOne && this.ADVANTAGE === this.scorePlayerTwo){
+		this.scorePlayerOne = this.THIRD_POINT;
+		this.scorePlayerTwo = this.THIRD_POINT;
+	}
+	if(this.ADVANTAGE === this.scorePlayerOne && this.ADVANTAGE !== this.scorePlayerTwo && this.THIRD_POINT !== this.scorePlayerTwo){
+		this.scorePlayerOne = this.WON;
+	}
+	if(this.ADVANTAGE === this.scorePlayerTwo && this.ADVANTAGE !== this.scorePlayerOne && this.THIRD_POINT !== this.scorePlayerOne){
+		this.scorePlayerTwo = this.WON;
+	}
 }
 
 Tennis.prototype.score = function(){
-	if(this.playerTwoHasAdvantage() && this.scorePlayerOne !== this.THIRD_POINT){
+	if(this.scorePlayerTwo === this.WON){
 		return "Player 2 Won";
 	}
-	if(this.playerOneHasAdvantage() && this.scorePlayerTwo !== this.THIRD_POINT){
+	if(this.scorePlayerOne === this.WON ){
 		return "Player 1 Won";
 	}
 	return this.scorePlayerOne + "-" + this.scorePlayerTwo;
@@ -34,7 +50,9 @@ Tennis.prototype.playerOneHasAdvantage = function(){
 }
 
 Tennis.prototype.calculateNewScore = function(oldScore){
-	if(oldScore === this.THIRD_POINT){
+	if(oldScore === this.ADVANTAGE){
+		oldScore = this.WON;
+	} else if(oldScore === this.THIRD_POINT){
 		oldScore = this.ADVANTAGE;
 	} else if(oldScore === this.SECOND_POINT){
 		oldScore = this.THIRD_POINT;
