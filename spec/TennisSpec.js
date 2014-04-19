c0c77341adc37fdb6bb6fc6c35d7b4382acd77c7
Alvaro Garcia <derecerca@gmail.com>
2014-04-19 14:32:27 +0200

    red: should score player one three times

--- spec/TennisSpec.js
+++ spec/TennisSpec.js
@@ -8,9 +8,18 @@ describe("Tennis", function(){
 		tennis.scoreOne();
 		expect("15-0").toBe(tennis.score());
 	});
+
 	it("should score player two", function(){
 		var tennis = new Tennis();
 		tennis.scoreTwo();
 		expect("0-15").toBe(tennis.score());
+	});
+
+	it("should score player one three times", function(){
+		var tennis = new Tennis();
+		tennis.scoreOne();
+		tennis.scoreOne();
+		tennis.scoreOne();
+		expect("40-0").toBe(tennis.score());
 	});
 });
