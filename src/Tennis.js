c39d33b8a4abdf7920ae86bf2ab0ae69236e231a
Alvaro Garcia <derecerca@gmail.com>
2014-04-19 14:28:39 +0200

    green

--- src/Tennis.js
+++ src/Tennis.js
@@ -1,1 +1,12 @@
-Tennis = function(){};
+var Tennis = function(){
+	this.scorePlayerOne = 0;
+	this.scorePlayerTwo = 0;
+};
+
+Tennis.prototype.scoreOne = function(){
+	this.scorePlayerOne += 15;
+}
+
+Tennis.prototype.score = function(){
+	return this.scorePlayerOne + "-" + this.scorePlayerTwo;
+}
