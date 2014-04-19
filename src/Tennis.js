1570331af5bba1d5705357f8ea80595ed52fe8d7
Alvaro Garcia <derecerca@gmail.com>
2014-04-19 14:31:09 +0200

    refactor: extract magic number 15 as FIRST_POINT

--- src/Tennis.js
+++ src/Tennis.js
@@ -1,14 +1,15 @@
 var Tennis = function(){
+	this.FIRST_POINT = 15;
 	this.scorePlayerOne = 0;
 	this.scorePlayerTwo = 0;
 };
 
 Tennis.prototype.scoreOne = function(){
-	this.scorePlayerOne += 15;
+	this.scorePlayerOne += this.FIRST_POINT;
 }
 
 Tennis.prototype.scoreTwo = function(){
-	this.scorePlayerTwo += 15;
+	this.scorePlayerTwo += this.FIRST_POINT;
 }
 
 Tennis.prototype.score = function(){
