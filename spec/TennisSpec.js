818d7c24478ef4c7381f4c02e376929704c3f238
Alvaro Garcia <derecerca@gmail.com>
2014-04-19 14:23:05 +0200

    red: should score player one

--- spec/TennisSpec.js
+++ spec/TennisSpec.js
@@ -2,4 +2,10 @@ describe("Tennis", function(){
 	it("should have a game", function(){
 		var tennis = new Tennis();
 	});
+
+	it("should score player one", function(){
+		var tennis = new Tennis();
+		tennis.scoreOne();
+		expect("15-0", toBe(tennis.score()));
+	});
 });
